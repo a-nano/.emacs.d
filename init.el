60e06f067000f8b3bab4f48e97f9583e0b0779fc
Akihide Nano <an74abc@gmail.com>
2016-09-18 21:03:29 +0900

    add files

--- init.el
+++ init.el
@@ -1,4 +1,9 @@
 (setq load-path (cons"~/.emacs.d/elisp" load-path))
+
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load)
+
--- inits/20-common-lisp.el
+++ inits/20-common-lisp.el
@@ -1,12 +1,18 @@
 ;; Common lisp
 
-;; sbclをデフォルトのCommon Lisp処理系に設定
-(setq inferior-lisp-program "sbcl")
-
-;; ~/.emacs.d/slimeをload-pathに追加
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/slime-2.18"))
-
 ;; SLIMEのロード
 (require 'slime)
-(slime-setup '(slime-repl slime-fancy slime-banner))
+(require 'ac-slime)
 
+;; sbclをデフォルトのCommon Lisp処理系に設定
+(setq inferior-lisp-program "sbcl")
+(setq slime-contribs '(slime-fancy))
+
+
+; (slime-setup '(slime-repl slime-fancy slime-banner slime-fuzzy slime-highlight-edits))
+(add-hook 'lisp-mode-hook 'slime-mode)
+(add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
