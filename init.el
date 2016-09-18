0be29a818c4e035849636e19e52bd31dbfe0420a
Akihide Nano <an74abc@gmail.com>
2016-09-18 21:19:27 +0900

    add to files

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (setq load-path (cons"~/.emacs.d/elisp" load-path))
 
-(package-initialize)
+(require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
 
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
---
+++ inits/20-package.el
@@ -0,0 +1,21 @@
+;; use package
+
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    ;; ここに使っているパッケージを書く。
+    slime
+    ac-slime
+    auto-complete
+    macrostep
+    popup
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
