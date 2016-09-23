572d2ca3621e438c7eeb2817c4ea60762772af4e
Akihide Nano <an74abc@gmail.com>
2016-09-23 09:14:05 +0900

    added setting

--- inits/00-init.el
+++ inits/00-init.el
@@ -127,3 +127,18 @@
 (setq eol-mnemonic-dos "(CRLF)")
 (setq eol-mnemonic-mac "(CR)")
 (setq eol-mnemonic-unix "(LF)")
+
+;; ファイルの再読込
+(defun revert-buffer-no-confirm (&optional force-reverting)
+  "Interactive call to revert-buffer. Ignoring the auto-save
+ file and not requesting for confirmation. When the current buffer
+ is modified, the command refuses to revert it, unless you specify
+ the optional argument: force-reverting to true."
+  (interactive "P")
+  ;;(message "force-reverting value is %s" force-reverting)
+  (if (or force-reverting (not (buffer-modified-p)))
+      (revert-buffer :ignore-auto :noconfirm)
+    (error "The buffer has been modified")))
+
+;; ファイルの最読込を割当
+(define-key global-map "\M-n" 'revert-buffer-no-confirm)
