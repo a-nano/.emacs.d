63fdf54458d00ddd2f3f69c4d2b8a2c4d399b3be
Akihide Nano <an74abc@gmail.com>
2017-02-10 08:53:26 +0900

    added

--- init.el
+++ init.el
@@ -8,3 +8,19 @@
 (setq init-loader-show-log-after-init nil)
 (init-loader-load)
 
+(put 'upcase-region 'disabled nil)
+(put 'set-goal-column 'disabled nil)
+
+
+;;; haskell—p
+
+(require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa-stable" . "http://stable.melpa.org/packages/")))))
---
+++ inits/20-haskell.el
@@ -0,0 +1,7 @@
+;; haskell-mode
+(autoload 'haskell-mode "haskell-mode")
+(autoload 'haskell-cabal "haskell-cabal")
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
+(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
+(setq haskell-program-name "/usr/bin/ghci")
